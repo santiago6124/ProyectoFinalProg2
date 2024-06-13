69f86cd7f73fe71577387381727295b0805e4334
facu-olivam <2406097@ucc.edu.ar>
2024-06-13 08:33:48 -0300

    .. coordenada

--- review/Coordenada.h
+++ review/Coordenada.h
@@ -1,18 +1,24 @@
+#pragma once
 
 #include <string>
 #include <iostream>
+#include <vector>
 #include "Barco.h"
 using namespace std;
 
-class Coordenada {
-private:
-    string letra;
-    int numero;
-    Barco* Barco = nullptr;
-    bool tocado = false;
+#ifndef Coordenada_H
+ #define Coordenada_H
+    class Coordenada {
+        private:
+            string letra;
+            int numero;
+            Barco* Barco = nullptr;
+            bool tocado = false;
 
-public:
-    // Constructor
-    Coordenada();
-    Coordenada(string letra, int numero) : letra(letra), numero(numero) {}
-};
+        public:
+            // Constructor
+            Coordenada();
+            Coordenada(string letra, int numero) : letra(letra), numero(numero) {}
+    };
+
+#endif
